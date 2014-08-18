73dd9a843ae5cf1dfe51fe9bdfbf073cce111a0e
skrypkaivan <ivan.skrypka@statiq.co>
2014-08-18 10:28:28 +0300

    allow to sign in only users who has following roles:ROLE_ADMIN,ROLE_MANAGER

--- app/scripts/directives/accessLevel.js
+++ app/scripts/directives/accessLevel.js
@@ -4,14 +4,13 @@ angular.module('itytApp').directive('accessLevel', ['$rootScope', function($root
     return {
       restrict: 'A',
       link: function(scope, element, attrs) {
-        var prevDisp = element.css('display');
         $rootScope.$watch('user.userRoles', function(userRoles) {
           var acceptedRoles = attrs.accessLevel.split(",");
           if(_.intersection(acceptedRoles, userRoles).length == 0) {
             element.css('display', 'none');
           }
           else {
-            element.css('display', prevDisp);
+            element.css('display', 'block');
           }
         });
       }
